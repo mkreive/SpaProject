b811d538570ab073819d8f6822b38d16c0e8215a
mkreive <mkreive@gmail.com>
2022-02-23 15:55:03 +0200

    links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Products from "./components/Products";
-import Welcome from "./components/Welcome";
+import MainHeader from "./components/MainHeader";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
 
 function App() {
     return (
-        <Routes>
-            <Route path="products" element={<Products />} />
-            <Route path="welcome" element={<Welcome />} />
-        </Routes>
+        <div>
+            <MainHeader />
+            <main>
+                <Routes>
+                    <Route path="products" element={<Products />} />
+                    <Route path="welcome" element={<Welcome />} />
+                </Routes>
+            </main>
+        </div>
     );
 }
 
