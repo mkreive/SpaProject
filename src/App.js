eeabc8d01e980237ea97df7569907f0823504ffe
mkreive <mkreive@gmail.com>
2022-02-23 15:07:42 +0200

    fixed

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Products from "./components/Products";
 import Welcome from "./components/Welcome";
 
 function App() {
     return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/welcome">Welcome</Link>
-                        </li>
-                        <li>
-                            <Link to="/products">Products</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                <Switch>
-                    <Route path="/welcome">
-                        <Welcome />
-                    </Route>
-                    <Route path="/products">
-                        <Products />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
+        <Routes>
+            <Route path="products" element={<Products />} />
+            <Route path="welcome" element={<Welcome />} />
+        </Routes>
     );
 }
 
