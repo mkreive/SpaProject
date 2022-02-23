792f89d72134c3b8099033c5438f687b04f1aa0e
mkreive <mkreive@gmail.com>
2022-02-23 16:11:41 +0200

    NavLink

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import classes from "./MainHeader.module.css";
 
 const MainHeader = () => {
@@ -7,10 +7,24 @@ const MainHeader = () => {
             <nav>
                 <ul>
                     <li>
-                        <Link to="/welcome">Welcome</Link>
+                        <NavLink
+                            className={(isActive) =>
+                                !isActive.isActive ? "" : classes.active
+                            }
+                            to="/welcome"
+                        >
+                            Welcome
+                        </NavLink>
                     </li>
                     <li>
-                        <Link to="/products">Products</Link>
+                        <NavLink
+                            className={(isActive) =>
+                                !isActive.isActive ? "" : classes.active
+                            }
+                            to="/products"
+                        >
+                            Products
+                        </NavLink>
                     </li>
                 </ul>
             </nav>
