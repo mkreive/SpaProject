6bf1205df2fd3b554b1800e848d6f0f6868669f5
mkreive <mkreive@gmail.com>
2022-02-23 14:55:55 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Products from "./components/Products";
+import Welcome from "./components/Welcome";
+
 function App() {
     return (
-        <div>
-            <h2>Let's get started!</h2>
-        </div>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/welcome">Welcome</Link>
+                        </li>
+                        <li>
+                            <Link to="/products">Products</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route path="/welcome">
+                        <Welcome />
+                    </Route>
+                    <Route path="/products">
+                        <Products />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
