73c4835d369781de959720fd68cb403d9934f2c9
mkreive <mkreive@gmail.com>
2022-02-24 11:21:50 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
 import Products from "./pages/Products";
 import Welcome from "./pages/Welcome";
 
@@ -12,6 +13,10 @@ function App() {
                 <Routes>
                     <Route path="products" element={<Products />} />
                     <Route path="welcome" element={<Welcome />} />
+                    <Route
+                        path="product-detail/:productId"
+                        element={<ProductDetail />}
+                    />
                 </Routes>
             </main>
         </div>
