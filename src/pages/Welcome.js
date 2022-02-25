f0f6341ef1807ba406f19413db88e9ddb5e5393e
mkreive <mkreive@gmail.com>
2022-02-25 12:38:39 +0200

    new user

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -6,7 +6,10 @@ const Welcome = () => {
         <section>
             <h1>The Welcome Page</h1>
             <Routes>
-                <Route path="/welcome/new-user"></Route>
+                <Route
+                    path="new-user"
+                    element={<p>Welcome, new user!</p>}
+                ></Route>
             </Routes>
         </section>
     );
