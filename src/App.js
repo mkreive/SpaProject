45a20af3ac9e080caaf5726d93b74f46dc07b97a
mkreive <mkreive@gmail.com>
2022-02-28 09:41:42 +0200

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import MainHeader from "./components/MainHeader";
 import ProductDetail from "./pages/ProductDetail";
 import Products from "./pages/Products";
 import Welcome from "./pages/Welcome";
+
+// // Lazy loading - uzloadins tik tada kai reikes to komponento
+// const Products = React.lazy(() => import("./pages/Products"));
 
 function App() {
     return (
@@ -17,7 +20,9 @@ function App() {
                         element={<Navigate replace to="/welcome" />}
                     />
                     <Route path="welcome/*" element={<Welcome />} />
+                    {/* <Suspense fallback={<p className="centered">Loading...</p>}> */}
                     <Route path="products/*" element={<Products />} />
+                    {/* </Suspense> */}
                     <Route
                         path="products/:productId"
                         element={<ProductDetail />}
