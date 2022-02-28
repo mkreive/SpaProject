5345dfac3b6d9ce41a566f5fc1af8947a471a35f
mkreive <mkreive@gmail.com>
2022-02-28 09:48:08 +0200

    building

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import MainHeader from "./components/MainHeader";
